c207c12357a6eb9bff4e00c33b1c2d6c539750fd
frankchiu12 <si-un_chiu@brown.edu>
2022-06-01 01:04:47 -0400

    cleaning up code

--- team_ranking.py
+++ team_ranking.py
@@ -1,9 +1,9 @@
-import sys
 from get_google_sheets import *
+import sys
 
 team_ranking_result_sheet = sheet.worksheet_by_title('Team Ranking')
-team_status_list = team_sheet.get_col(3, include_tailing_empty=False)
-del team_status_list[0:1]
+# list of team status (deleting the column header)
+team_status_list = team_sheet.get_col(3, include_tailing_empty=False)[1:]
 status_list = ['ESL', 'Novice']
 
 class TeamRanking:
@@ -15,8 +15,8 @@ class TeamRanking:
         self.team_to_score = {}
         self.team_to_result = {}
         self.no_ballot_team_list = []
-        self.get_score_and_result()
-        self.populate_row_list_dictionary()
+        self.read_round_rows()
+        self.populate_team_to_score_and_team_to_result_dictionary()
 
         if self.check_if_all_ballots_are_in():
             self.team_to_win = {}
@@ -24,26 +24,33 @@ class TeamRanking:
             self.team_to_average = {}
             self.team_to_win_count = {}
             self.team_to_win_count_and_average = {}
-            self.win_count_list = []
+
+            # lists to write into team_ranking_result_sheet
             self.score_list = []
+            self.win_count_list = []
             self.average_list = []
             self.ranking_number_list = []
             self.ranked_team_list = []
+
+            # for ranking ESL and Novice teams
             self.ranked_team_to_row = {}
             self.status_to_team = {}
         else:
             sys.exit()
 
-    def get_score_and_result(self):
+    # read the rows of scores and results for each round
+    def read_round_rows(self):
         for i in range(1, number_of_teams + 1):
             self.round_row_list.append(self.round_sheet.get_row(i + 1, include_tailing_empty=False))
 
-    def populate_row_list_dictionary(self):
+    def populate_team_to_score_and_team_to_result_dictionary(self):
         for row in self.round_row_list:
             team = row[0]
             if team not in self.team_to_score:
+                # int(row[1: 2][0]) is the score
                 self.team_to_score[team] = [int(row[1: 2][0])]
             if team not in self.team_to_result:
+                # row[2: 4] is the result
                 self.team_to_result[team] = [row[2: 4]]
 
     def check_if_all_ballots_are_in(self):
@@ -55,16 +62,85 @@ class TeamRanking:
             return False
         return True
 
-    def write_average_to_sheet(self):
-        team_ranking_result_sheet.update_col(1, self.ranking_number_list)
-        team_ranking_result_sheet.update_col(2, self.ranked_team_list)
-        team_ranking_result_sheet.update_col(self.number_of_rounds + 4, self.average_list)
+    def loop(self):
+
+        # running the class for each round, adding to self.team_to_score and self.team_to_result
+        for i in range(1, self.number_of_rounds + 1):
+            team_ranking = TeamRanking(i, self.number_of_rounds)
+            if i != 1:
+                for team in team_list:
+                    self.team_to_score[team] = self.team_to_score[team] + team_ranking.team_to_score[team]
+                    self.team_to_result[team] = self.team_to_result[team] + team_ranking.team_to_result[team]
+
+        self.populate_team_to_win_loss_average_dictionaries()
+        self.populate_team_to_win_count_win_count_and_average_dictionaries()
+        self.rank()
+
+        # lists to write to team_ranking_result_sheet
+        round_column_header_list = []
+        self.ranking_number_list.append('RANKING')
+        self.ranked_team_list.append('TEAM')
+        counter = 1
+
+        for round in range(1, self.number_of_rounds + 1):
+            round_column_header_list = round_column_header_list + ['Round ' + str(round)]
+        self.score_list.append(round_column_header_list)
+        self.win_count_list.append('WINS')
+        self.average_list.append('AVERAGE')
+
+        for team in self.team_to_win_count_and_average:
+            self.ranking_number_list.append(counter)
+            self.ranked_team_list.append(team)
+            self.win_count_list.append(self.team_to_win_count[team])
+            self.score_list.append(self.team_to_score[team])
+            self.average_list.append(self.team_to_average[team])
+            counter += 1
+        
+        self.write_score_to_sheet()
+        self.write_win_count_to_sheet()
+        self.write_average_to_sheet()
+
+        DataRange('A1','Z1', worksheet=team_ranking_result_sheet).apply_format(bold)
+
+        self.populate_ranked_team_to_row_dictionary()
+        self.rank_on_status()
+
+    def populate_team_to_win_loss_average_dictionaries(self):
+        for team in team_list:
+            # win
+            if team not in self.team_to_win:
+                self.team_to_win[team] = [round[0] for round in self.team_to_result[team]]
+            # loss
+            if team not in self.team_to_loss:
+                self.team_to_loss[team] = [round[1] for round in self.team_to_result[team]]
+            # average
+            if team not in self.team_to_average:
+                self.team_to_average[team] = sum(self.team_to_score[team])/self.number_of_rounds
+
+    def populate_team_to_win_count_win_count_and_average_dictionaries(self):
+        for team in team_list:
+            # win count
+            if team not in self.team_to_win_count:
+                self.team_to_win_count[team] = len([result for result in self.team_to_win[team] if result != 'None'])
+            # wint count and average
+            if team not in self.team_to_win_count_and_average:
+                self.team_to_win_count_and_average[team] = [self.team_to_win_count[team]] + [self.team_to_average[team]]
+
+    # rank self.team_to_win_count_and_average first on win count then on average
+    def rank(self):
+        sorted_win_count_and_average_list = sorted(self.team_to_win_count_and_average.items(), key=lambda x: (x[1], x[1][1]), reverse=True)
+        self.team_to_win_count_and_average = self.convert(sorted_win_count_and_average_list, self.team_to_win_count_and_average)
 
     def write_score_to_sheet(self):
         team_ranking_result_sheet.update_values('C1', self.score_list)
 
     def write_win_count_to_sheet(self):
         team_ranking_result_sheet.update_col(self.number_of_rounds + 3, self.win_count_list)
+    
+    def write_average_to_sheet(self):
+        team_ranking_result_sheet.update_col(1, self.ranking_number_list)
+        team_ranking_result_sheet.update_col(2, self.ranked_team_list)
+        team_ranking_result_sheet.update_col(self.number_of_rounds + 4, self.average_list)
 
     def convert(self, tuple, dictionary):
         dictionary = dict(tuple)
@@ -130,62 +206,8 @@ class TeamRanking:
                 novice_team_ranking_result_sheet.update_values('B2', novice_team_row)
                 DataRange('A1','Z1', worksheet=novice_team_ranking_result_sheet).apply_format(bold)
 
-    def loop(self):
-
-        for i in range(1, self.number_of_rounds + 1):
-            team_ranking = TeamRanking(i, self.number_of_rounds)
-            if i != 1:
-                for team in team_list:
-                    self.team_to_score[team] = self.team_to_score[team] + team_ranking.team_to_score[team]
-                    self.team_to_result[team] = self.team_to_result[team] + team_ranking.team_to_result[team]
-
-        for i in range(number_of_teams):
-            if team_list[i] not in self.team_to_average:
-                self.team_to_average[team_list[i]] = sum(self.team_to_score[team_list[i]])/self.number_of_rounds
-            if team_list[i] not in self.team_to_win:
-                self.team_to_win[team_list[i]] = [round[0] for round in self.team_to_result[team_list[i]]]
-            if team_list[i] not in self.team_to_loss:
-                self.team_to_loss[team_list[i]] = [round[1] for round in self.team_to_result[team_list[i]]]
-
-        for team in team_list:
-            if team not in self.team_to_win_count:
-                self.team_to_win_count[team] = len([result for result in self.team_to_win[team] if result != 'None'])
-            if team not in self.team_to_win_count_and_average:
-                self.team_to_win_count_and_average[team] = [self.team_to_win_count[team]] + [self.team_to_average[team]]
-
-        sorted_win_count_and_average_list = sorted(self.team_to_win_count_and_average.items(), key=lambda x: (x[1], x[1][1]), reverse=True)
-
-        self.team_to_win_count_and_average = self.convert(sorted_win_count_and_average_list, self.team_to_win_count_and_average)
-
-        self.ranking_number_list.append('RANKING')
-        self.ranked_team_list.append('TEAM')
-        counter = 1
-        round_column_header_list = []
-        for round in range(1, self.number_of_rounds + 1):
-            round_column_header_list = round_column_header_list + ['Round ' + str(round)]
-        self.score_list.append(round_column_header_list)
-        self.win_count_list.append('WINS')
-        self.average_list.append('AVERAGE')
-
-        for team in self.team_to_win_count_and_average:
-            self.ranking_number_list.append(counter)
-            self.ranked_team_list.append(team)
-            self.win_count_list.append(self.team_to_win_count[team])
-            self.score_list.append(self.team_to_score[team])
-            self.average_list.append(self.team_to_average[team])
-            counter += 1
-        
-        self.write_score_to_sheet()
-        self.write_win_count_to_sheet()
-        self.write_average_to_sheet()
-
-        DataRange('A1','Z1', worksheet=team_ranking_result_sheet).apply_format(bold)
-
-        self.populate_ranked_team_to_row_dictionary()
-        self.rank_on_status()
-
 team_ranking_result_sheet.clear('A1')
 team_ranking = TeamRanking(1, 4)
 team_ranking.loop()
 
-# REPL
+# wins and losses

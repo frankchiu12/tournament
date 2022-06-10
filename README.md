11f60d3093e1f0a7597356e243b61434aa2146cb
frankchiu12 <si-un_chiu@brown.edu>
2022-06-09 22:18:48 -0400

    code finished

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 https://docs.google.com/spreadsheets/d/1Oc3Nhj1CgxcMAJXk08IJQ0E9S4YqE8Eqnb7wqY_UZQ0/edit?usp=sharing
 
-https://cloud.google.com/blog/products/application-development/auto-generating-google-forms
-
-https://developers.google.com/apps-script/guides/projects
-
 Google Sheets API
--- REPL.py
+++ REPL.py
@@ -4,7 +4,9 @@ import os
 term_size = os.get_terminal_size()
 
 outrounds_sheet = sheet.worksheet_by_title('Outrounds')
+# list of teams in the order of their seed (deleting the column header)
 team_and_seed_list = outrounds_sheet.get_col(1, include_tailing_empty=False)
+# list of weights
 weight_list = outrounds_sheet.get_col(2, include_tailing_empty=False)
 team_to_weight = {}
 weight_to_team = {}
@@ -14,7 +16,7 @@ def check_if_seeding_valid():
     if len(team_and_seed_list) == 16 or len(team_and_seed_list) == 8:
         return True
 
-def assign_seeding_weights():
+def populate_team_to_weight_and_weight_to_team_dictionaries():
     for i in range(len(team_and_seed_list)):
         if team_and_seed_list[i] not in team_to_weight:
             team_to_weight[team_and_seed_list[i]] = int(weight_list[i])
@@ -23,9 +25,11 @@ def assign_seeding_weights():
             weight_to_team[weight] = team
 
 def get_and_populate_matchup():
-    for i in range(int(len(team_and_seed_list)/2)):            
+    for i in range(int(len(team_and_seed_list)/2)):      
+        # i + 1 because you want to start at 1      
         if i + 1 not in matchup:
             matchup[i + 1] = []
+        # you add the team counting from the beginning and the end of the team_and_seed_list with the same index (for example: 1 and 16, 2 and 15, 3 and 14, etc.)
         list_to_add = [team_and_seed_list[i], team_and_seed_list[len(team_and_seed_list) - i - 1]]
         random.shuffle(list_to_add)
         matchup[i + 1] = matchup[i + 1] + list_to_add
@@ -34,16 +38,20 @@ def determine_winner_manual():
     for i in range(1, int(len(matchup)) + 1):
         print('\n' + 'For the matchup between seed ' + str(i) + ' and seed ' + str(len(matchup)*2 - int(i - 1)) + ', we have teams ' + str(matchup[i]) + '. Who won?')
         y = input('\n' + 'team> ')
+        # if the input does not match any of the teams in the matchup
         while not any(y.lower() in team.lower() for team in matchup[i]):
             print('Your input does not match any of the teams. Try again.')
             y = input('\n' + 'team> ')
+        # store the first team that matches the input
         team_to_return = next((team for team in matchup[i] if y.lower() in team.lower()), None)
         matchup[i] = team_to_return
+        # if the length of the dictionary is 1 and the list of that key is greater than 1 (the list has not been altered yet)
         if len(matchup) == 1 and len(matchup[1]) > 1:
             print(str(team_to_return) + ' is the CHAMPION! \n')
         else:
             print(str(team_to_return) + ' has won the matchup.')
 
+    # redo matchup
     if len(matchup) == 1:
         matchup[1] = [matchup[1]]
     for i in range(1, int(len(matchup)/2 + 1)):
@@ -53,6 +61,7 @@ def determine_winner_manual():
     if half_of_matchup_number == 0:
         half_of_matchup_number = 1
 
+    # if the key is greater than half of the matchups left, you pop the key
     for i in matchup.copy():
         if i > half_of_matchup_number:
             matchup.pop(i)
@@ -62,20 +71,23 @@ def determine_winner_manual():
         print('Next round!')
         print('\n' + '=' * term_size.columns)
         determine_winner_manual()
-    if len(matchup) == 1:
-        if len(matchup[1]) > 1:
-            print('\n' + '=' * term_size.columns + '\n')
-            print('Next round!')
-            print('\n' + '=' * term_size.columns)
-            determine_winner_manual()
+    # need to call determine_winner_manual() one last time for the finals
+    if len(matchup) == 1 and len(matchup[1]) > 1:
+        print('\n' + '=' * term_size.columns + '\n')
+        print('Next round!')
+        print('\n' + '=' * term_size.columns)
+        determine_winner_manual()
 
 def determine_winner_auto():
     for i in range(1, len(matchup) + 1):
         team_list = matchup[i]
+        # sorted based on weights so the higher seeded team is the first item in the list
         sorted_team_list = sorted(team_list, key=lambda x: team_to_weight[x], reverse=True)
+        # weight_difference is guaranteed to be positive because of the sorting
         weight_difference = team_to_weight[sorted_team_list[0]] - team_to_weight[sorted_team_list[1]]
         probability = 0
 
+        # assign win probabilities based on the weight_difference
         if weight_difference >= 50:
             probability = 99
         elif weight_difference >= 40:
@@ -97,6 +109,7 @@ def determine_winner_auto():
         else:
             matchup[i] = sorted_team_list[1]
 
+    # redo matchup
     if len(matchup) == 1:
         matchup[1] = [matchup[1]]
     for i in range(1, int(len(matchup)/2 + 1)):
@@ -106,6 +119,7 @@ def determine_winner_auto():
     if half_of_matchup_number == 0:
         half_of_matchup_number = 1
 
+    # if the key is greater than half of the matchups left, you pop the key
     for i in matchup.copy():
         if i > half_of_matchup_number:
             matchup.pop(i)
@@ -114,11 +128,12 @@ def determine_winner_auto():
 
     if len(matchup) > 1:
         determine_winner_auto()
-    if len(matchup) == 1:
-        if len(matchup[1]) > 1:
-            determine_winner_auto()
+    # need to call determine_winner_auto() one last time for the finals
+    if len(matchup) == 1 and len(matchup[1]) > 1:
+        determine_winner_auto()
 
 def main():
+    # clear the terminal
     os.system('cls' if os.name == 'nt' else 'clear')
     print('\n' + 'Use this to simulate a tournament. To manually input results, type manual. To autogenerate results, type auto.')
     x = input('\n' + 'command> ')
@@ -137,10 +152,8 @@ def main():
 
 if __name__ == '__main__':
     if check_if_seeding_valid():
-        assign_seeding_weights()
+        populate_team_to_weight_and_weight_to_team_dictionaries()
         get_and_populate_matchup()
     else:
         print('The number of teams is not valid.')
     main()
-
-# visualizer (https://github.com/cristiean/bracket)
